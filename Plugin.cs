bf276756022c69ad3f010ba48ee1ca2b0a46d54d
Benedikt Werner <1benediktwerner@gmail.com>
2022-07-23 12:50:59 +0200

    Fix island shader when board too large

--- Plugin.cs
+++ Plugin.cs
@@ -18,12 +18,37 @@ namespace LargerBoard
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
 
-        [HarmonyPatch(typeof(WorldManager), "DetermineTargetWorldSize")]
+        [HarmonyPatch(typeof(WorldManager), nameof(WorldManager.DetermineTargetWorldSize))]
         [HarmonyPrefix]
-        private static void WorldManager__DetermineTargetWorldSize__Prefix(ref bool __runOriginal, WorldManager __instance, GameBoard board, out float __result)
+        private static void DetermineTargetWorldSize(ref bool __runOriginal, WorldManager __instance, GameBoard board, out float __result)
         {
             __runOriginal = false;
             __result = Mathf.Clamp(__instance.CardCapIncrease(board) * 0.03f, 0.15f, maxSize.Value);
+        }
+
+        private static GameBoard currentBoard = null;
+        private static Material currentBoardMaterial = null;
+
+        [HarmonyPatch(typeof(GameBoard), nameof(GameBoard.Update))]
+        [HarmonyPrefix]
+        private static void FixShader(GameBoard __instance)
+        {
+            if (__instance.IsCurrent)
+            {
+                if (currentBoard != __instance)
+                {
+                    currentBoard = __instance;
+                    currentBoardMaterial = __instance.GetComponent<MeshRenderer>().material;
+                }
+                if (currentBoard.WorldSizeIncrease < 2.5f)
+                {
+                    currentBoardMaterial.SetVector("_Rect", new Vector4(4.3f, 0, 2.47f, -0.43f));
+                }
+                else
+                {
+                    currentBoardMaterial.SetVector("_Rect", new Vector4(1.8f + currentBoard.WorldSizeIncrease, 0, currentBoard.WorldSizeIncrease, -0.43f));
+                }
+            }
         }
     }
 }
